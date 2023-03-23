615d34f6b5436e0b850795b6ce1d6e73a5f47ea1
Jado <jade.bouiges@edu.itescia.fr>
2023-03-23 19:49:54 +0100

    add particuules end screen

--- Assets/Scripts/Menu/EndingManager.cs
+++ Assets/Scripts/Menu/EndingManager.cs
@@ -6,7 +6,9 @@ public class EndingManager : MonoBehaviour
 {
     [SerializeField] private Text textEnd;
     public GameManager _gameManager;
-
+    [SerializeField] GameObject _fireworks;
+    [SerializeField] GameObject _rain;
+    [SerializeField] GameObject bg;
 
     public static EndingManager instance;
 
@@ -19,7 +21,7 @@ public class EndingManager : MonoBehaviour
         }
 
         instance = this;
-
+        
         _gameManager = GameObject.Find("GameManager").GetComponent<GameManager>();
 
     }
@@ -41,10 +43,18 @@ public class EndingManager : MonoBehaviour
     private void Wining() 
     {
         textEnd.text = "YOU WIN";
+        _fireworks.SetActive(true);
+        _rain.SetActive(false);
+
     }
     private void Losing() 
     {
         textEnd.text = "YOU LOSE";
+        _rain.SetActive(true);
+        _fireworks.SetActive(false);
+
+        bg.GetComponent<SpriteRenderer>().color = new Color32(114, 114, 114, 225);
+
     }
 
     public void ReturnMainMenu()
