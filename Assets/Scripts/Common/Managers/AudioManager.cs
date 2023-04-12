da24e41d78c033617f944df2d56e286d14768861
Jado <jade.bouiges@edu.itescia.fr>
2023-04-12 11:29:45 +0200

    fix divers bugs (froggy panel/order layer/legal/credits)

--- Assets/Scripts/Common/Managers/AudioManager.cs
+++ Assets/Scripts/Common/Managers/AudioManager.cs
@@ -5,12 +5,12 @@ using UnityEngine;
 public class AudioManager : MonoBehaviour
 {
 
-    public AudioClip[] playlist;
+   // public AudioClip[] playlist;
     public AudioSource audioSource;
     // Start is called before the first frame update
     void Start()
     {
-        audioSource.clip = playlist[0];
+        //audioSource.clip = playlist[0];
         audioSource.Play();
     }
 
